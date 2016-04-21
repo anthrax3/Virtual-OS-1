ca8ac38110463b3ab8508754e08b9526b9d89883
Bogdan Pechyorin <iosgamer.awesome@gmail.com>
2016-04-21 09:29:41 -0700

    Fixed Settings.cs

---
+++ Virtual OS/Virtual OS/MainSource/Settings.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Virtual_OS.MainSource
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TopBar = new System.Windows.Forms.Panel();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.CloseButton = new MetroFramework.Controls.MetroLink();
+            this.TopBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TopBar
+            // 
+            this.TopBar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.TopBar.Controls.Add(this.CloseButton);
+            this.TopBar.Controls.Add(this.TitleLabel);
+            this.TopBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopBar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.TopBar.Location = new System.Drawing.Point(0, 0);
+            this.TopBar.Name = "TopBar";
+            this.TopBar.Size = new System.Drawing.Size(798, 41);
+            this.TopBar.TabIndex = 0;
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Location = new System.Drawing.Point(12, 9);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(59, 17);
+            this.TitleLabel.TabIndex = 1;
+            this.TitleLabel.Text = "Settings";
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.AutoSize = true;
+            this.CloseButton.Location = new System.Drawing.Point(713, 9);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(82, 27);
+            this.CloseButton.TabIndex = 5;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.CloseButton.UseCustomBackColor = true;
+            this.CloseButton.UseSelectable = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 581);
+            this.Controls.Add(this.TopBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Settings";
+            this.TopBar.ResumeLayout(false);
+            this.TopBar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TopBar;
+        private System.Windows.Forms.Label TitleLabel;
+        public MetroFramework.Controls.MetroLink CloseButton;
+    }
+}
---
+++ Virtual OS/Virtual OS/MainSource/Settings.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Virtual_OS.MainSource
+{
+    public partial class Settings : Form
+    {
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public Settings()
+        {
+            InitializeComponent();
+
+            TopBar.MouseMove += TopBar_MouseMove;
+
+            // Setup.UseSelectable = false;
+        }
+
+        private void TopBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void ExitAppBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Setup_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
