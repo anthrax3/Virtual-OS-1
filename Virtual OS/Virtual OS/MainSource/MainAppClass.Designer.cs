c0df093c1cf35b1a11773ef45bb93048eff9d9ac
Bogdan Pechyorin <iosgamer.awesome@gmail.com>
2016-04-21 09:28:52 -0700

    Fixed MainApClass.cs

---
+++ Virtual OS/Virtual OS/MainSource/MainAppClass.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace Virtual_OS.MainSource
+{
+    partial class MainAppClass
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainAppClass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 612);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainAppClass";
+            this.ShowIcon = false;
+            this.Text = "MainAppClass";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
